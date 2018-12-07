1d74fe2786eeddce4fb459724234dba3fb693201
sauvikatinnofied <sauvik.dolui@innofied.com>
2018-12-07 15:39:08 +0530

    Initial Commit

---
+++ JazzyDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JazzyDemo
+//
+//  Created by Sauvik Dolui on 07/12/18.
+//  Copyright © 2018 Innofied Solution Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
